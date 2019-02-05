e5ebabe4988b0ff3e970c6e3fa3353e3d6ced7e1
Bruno Belorgey <bruno.belorgey@laposte.net>
2019-02-05 16:08:42 +0100

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,34 @@ import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
 
 import { FormsModule } from '@angular/forms';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent},
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     Component1Component,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
