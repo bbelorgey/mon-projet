4f0e890906d4ce32e1daf8e6034e4bc2d29c69a8
Bruno Belorgey <bruno.belorgey@laposte.net>
2019-02-05 12:30:27 +0100

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mon-projet';
+export class AppComponent implements OnInit {
+  isAuth = false;
+
+  lastUpdate = new Promise(
+    (resolve,reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+        resolve(date)
+      }, 4000);
+    }
+  );
+
+  appareils : any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    )
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+
+  onTeindre() {
+    this.appareilService.switchOffAll();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
+import { AppareilService } from './services/appareil.service';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    AppareilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
